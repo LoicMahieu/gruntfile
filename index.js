c4b1fba43882bb6e57c4a2c8b78efadbe448348e
Loïc Mahieu <mahieuloic@gmail.com>
2014-06-20 18:03:56 +0200

    Adds the ability to pass a given more arguments to `fn`

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ module.exports = function(moduleCWD, fn) {
     // Merge override with parent
     overridecfg = overridecfg || {};
     var parentcfg = grunt.config.getRaw();
+    var args = [grunt, parentCWD].concat(Array.prototype.slice.call(arguments, 2));
     if (Object.keys(overridecfg).length > 0) {
       parentcfg = grunt.util._.merge(parentcfg, overridecfg);
       grunt.initConfig(parentcfg);
@@ -31,7 +32,7 @@ module.exports = function(moduleCWD, fn) {
 
     // Adjust CWD if has a parentCWD
     if (parentCWD) process.chdir(moduleCWD);
-    var res = fn(grunt, parentCWD);
+    var res = fn.apply(null, args);
     if (parentCWD) process.chdir(parentCWD);
     return (res != null) ? res : Object.keys(grunt.config.getRaw());
   };
